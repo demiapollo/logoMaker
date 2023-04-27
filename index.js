61037c68661e30d47fded38b298816066a0f9ba3
demiapollo <apollodemirel67@gmail.com>
2023-04-27 10:54:30 -0700

    fixed errors

--- index.js
+++ index.js
@@ -2,27 +2,32 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const questions = require("./lib/questions");
 const fileName = "./examples/logo.svg";
-const setShape = require("./lib/setShape");
 
-// function to create a new svg
-function createSvg(response) {
-  const svg = setShape(response);
-  fs.writeFile(fileName, svg, (err) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-  });
-}
+const SVG = require("./lib/svg");
+const { Circle, Triangle, Square } = require("./lib/shapes"); 
 
-// function to initialize
-function init() {
-  inquirer
-    .prompt(questions)
-    .then((response) => {
-      createSvg(response);
-    })
-    .catch((err) => {
-      console.log(err);
+inquirer.prompt(questions).then((response) => {
+    let shape;
+    if (response.shape === "circle") {
+        shape = new Circle();
+    } 
+    if (response.shape === "triangle") {
+        shape = new Triangle();
+    }
+    if (response.shape === "square") {
+        shape = new Square();
+    }
+    shape.setColor(response.shapeColor);
+
+    const svg = new SVG();
+    svg.setText(response.text, response.textColor);
+    svg.setShape(shape);
+
+    fs.writeFile(fileName, svg.render(), (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Success!");
     });
-}
 
-init();
+});
